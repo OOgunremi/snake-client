df6947cb39d471e93933bc410479c6fdb4273941
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-03-16 22:48:43 -0600

    updated with more codes

--- input.js
+++ input.js
@@ -1,18 +1,15 @@
 
 let connection;
-
 // setup interface to handle user input from stdin
 const handleUserInput = function(data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
+  if (data === '\u0003') process.exit();
   if (data === 'w') connection.write("Move: up");
   if (data === 'a') connection.write("Move: left");
   if (data === 's') connection.write("Move: down");
   if (data === 'd') connection.write("Move: right");
-
+  if (data === 'p') connection.write('Say: Sup');
+  if (data === 'q') connection.write('Say: Holla');
 };
-
 const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
@@ -22,5 +19,4 @@ const setupInput = function(conn) {
   stdin.on("data", handleUserInput);
   return stdin;
 };
-
 module.exports = {setupInput};
